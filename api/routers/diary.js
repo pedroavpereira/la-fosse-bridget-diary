10d6529d5e403023e56e81df9351caed82ea9483
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-02 10:28:44 +0100

    Add dev script

--- api/routers/diary.js
+++ api/routers/diary.js
@@ -4,5 +4,9 @@ const diaryController = require("../controllers/diaries");
 const diaryRouter = Router();
 
 diaryRouter.get("/", diaryController.index);
+diaryRouter.get("/:id", diaryController.show);
+diaryRouter.post("/", diaryController.create);
+diaryRouter.patch("/:id", diaryController.update);
+diaryRouter.delete("/:id", diaryController.destroy);
 
 module.exports = diaryRouter;
