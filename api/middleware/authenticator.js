171d60d98dbc80b117ea3cd05851dfbff6aeddc5
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-02 14:50:02 +0100

    Make posts only accessible to the creatpr

--- api/middleware/authenticator.js
+++ api/middleware/authenticator.js
@@ -1,25 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-function authenticator(req, res, next){
-    const token = req.headers.get('authorization')
+function authenticator(req, res, next) {
+  const token = req.headers.authorization;
 
-    
-    if (!token) {
-        res.status(401).json({ success: false, error: "Unable to authenticate" });
-      }
-    //   guard clause
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Unable to authenticate" });
+  }
+  //   guard clause
 
-      jwt.verify(token, process.env.SECRET_TOKEN, async (err, data) => {
-        if (err) {
-          res.status(401).json({ success: false, error: "Unable to authenticate" });
-        }
-       
-        req.user = data.username;
-        next();
-       
-      });
+  jwt.verify(token, process.env.SECRET_TOKEN, async (err, data) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Unable to authenticate" });
+    }
 
+    req.user_id = data.user_id;
+    next();
+  });
 }
-
 
 module.exports = authenticator;
