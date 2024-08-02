51284ce32aa2e128b27e6fedee051b8d56a82fbc
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-02 09:52:43 +0100

    Create skeleton of Entry model

--- api/controllers/diaries.js
+++ api/controllers/diaries.js
@@ -1,8 +1,8 @@
-const Diary = require("../models/Diary");
+const Entry = require("../models/Entry");
 
 const index = async (req, res) => {
   try {
-    const posts = await Diary.showAll();
+    const posts = await Entry.showAll();
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
     res.status(404).json({ sucess: false, error: err.message });
@@ -12,7 +12,7 @@ const index = async (req, res) => {
 const show = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
-    const posts = await Diary.showOneById(id);
+    const posts = await Entry.showOneById(id);
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
     res.status(404).json({ success: false, error: err.message });
@@ -22,7 +22,7 @@ const show = async (req, res) => {
 const create = async (req, res) => {
   try {
     const data = req.body;
-    const newPost = await Diary.create(data);
+    const newPost = await Entry.create(data);
     res.status(201).json({ success: true, data: newPost });
   } catch (err) {
     res.status(400).json({ success: true, error: err.message });
@@ -33,7 +33,7 @@ const update = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
     const data = req.body;
-    const oldPost = await Diary.showOneById(id);
+    const oldPost = await Entry.showOneById(id);
     const updatedPost = oldPost.update(data);
     res.status(200).json({ success: true, data: updatedPost });
   } catch (err) {
@@ -44,7 +44,7 @@ const update = async (req, res) => {
 const destroy = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
-    const post = await Diary.showOneById(id);
+    const post = await Entry.showOneById(id);
     const deletedPost = post.update(data);
     res.sendStatus(204);
   } catch (err) {
---
+++ api/models/Entry.js
@@ -0,0 +1,50 @@
+const db = require("../database/connect.js");
+
+class Entry {
+  constructor(post_id, title, content, user_id) {
+    this.post_id = post_id;
+    this.title = title;
+    this.content = content;
+    this.user_id = user_id;
+  }
+
+  static async showAll() {
+    const posts = await db.query("Select * from posts;");
+
+    if (posts.rows.length === 0) throw new Error("No entry found");
+
+    return posts.rows.map((p) => new Entry());
+  }
+
+  static async showOneById(id) {
+    const post = await db.query("Select * from posts WHERE post_id = $1", [id]);
+
+    if (post.rows.length === 0) throw new Error("No entry found");
+
+    return new Entry(post.rows);
+  }
+
+  static async create(data) {
+    const newPost = await db.query(
+      "INSERT INTO posts (title,content,user_id) VALUES ($1 , $2 , $3 ) RETURNING * ;",
+      [data.title, data.content, data.user_id]
+    );
+
+    if (newPost.rows.length === 0) throw new Error("Error creating a new post");
+
+    return new Entry(newPost.rows);
+  }
+
+  async destroy() {
+    const deletedPost = db.query("DELETE FROM posts WHERE post_id = $1 ", [
+      this.id,
+    ]);
+
+    if (deletedPost.rows.length === 0)
+      throw new Error("Error creating a new post");
+
+    return new Entry(deletedPost.rows);
+  }
+}
+
+module.exports = Entry;
